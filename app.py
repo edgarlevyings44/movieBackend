14b0d5d0bb42b176ba2d6e7c317a52d85c893e31
edgarlevyings44 <edgarlevy81@gmail.com>
2024-02-20 16:35:06 +0300

    code clean up

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configure the Postgres connection
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://levy:P3f5ZTBxqLuukpXcaTLxFj26DoTtHh1l@dpg-cnaaa96v3ddc73d9b7p0-a.oregon-postgres.render.com/dbmovie_kcu8'
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+
 
 # Create the SQLAlchemy instance with the engine
 db = SQLAlchemy(app)
