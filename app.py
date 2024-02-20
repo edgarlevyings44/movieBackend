b53145018e48812bfaf859d1dbba5509029140b9
OCHIENGHerman <ochiengherman32@gmail.com>
2024-02-20 17:08:45 +0300

    project:running

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.debug = True
 
 # adding configuration for using a sqlite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://avnadmin:AVNS_CfgBHmKoHZYY7KpD10G@pg-16ee9791-edgarlevy81-b97b.a.aivencloud.com:26711/defaultdb?sslmode=require'
 # Creating an SQLAlchemy instance
 db = SQLAlchemy(app)
 @app.route('/')
