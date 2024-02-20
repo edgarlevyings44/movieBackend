9e451d7b8a2d104096564fca1264c10881f5c1e4
edgarlevyings44 <edgarlevy81@gmail.com>
2024-02-20 17:08:54 +0300

    Postgresql fix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.debug = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://levy:P3f5ZTBxqLuukpXcaTLxFj26DoTtHh1l@dpg-cnaaa96v3ddc73d9b7p0-a.oregon-postgres.render.com/dbmovie_kcu8'
 
 
 db = SQLAlchemy(app)
